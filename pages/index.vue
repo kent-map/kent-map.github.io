9e76e5c236381422de01687f3e7a21351003d73e
Ron Snyder <ronald.snyder@ithaka.org>
2019-12-24 10:54:03 -0500

    Updated text

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Kent Maps Online
+          Welcome to Kent Maps Online
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
